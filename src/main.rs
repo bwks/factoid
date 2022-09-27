e1f88c4693a35deab27c6ee4bd81aeadc8df6bd3
Brad Searle <bradleysearle@gmail.com>
2022-09-27 19:14:28 +1000

    more stubs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use std::env;
 
+#[derive(Default)]
+struct SysInfo {
+    arch: String,
+    os: String,
+    os_family: String,
+}
+
 fn main() {
-    let arch = env::consts::ARCH;
-    let os = env::consts::OS;
-    let os_family = env::consts::FAMILY;
-    println!("arch: {}", arch);
-    println!("os: {}", os);
-    println!("os_family: {}", os_family);
+    let mut sysinfo = SysInfo::default();
+
+    sysinfo.arch = env::consts::ARCH.to_owned();
+    sysinfo.os = env::consts::OS.to_owned();
+    sysinfo.os_family = env::consts::FAMILY.to_owned();
+
+    println!("arch: {}", sysinfo.arch);
+    println!("os: {}", sysinfo.os);
+    println!("os_family: {}", sysinfo.os_family);
 }
